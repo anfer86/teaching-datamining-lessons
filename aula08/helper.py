a3119ad8320ac3a3af21489720145bb2ebba0631
anfer86 <anfer86@gmail.com>
2019-10-08 22:50:01 -0300

    avaliacao de modelos

--- aula08/helper.py
+++
@@ -1,6 +0,0 @@
-
-
-def confusion_matrix_df(confusion_matrix):
-    return pd.DataFrame(data = confusion_matrix, 
-                        index = ['observed_negative','observed_positive'], 
-                        columns = ['predicted_negative','predicted_positive'])
